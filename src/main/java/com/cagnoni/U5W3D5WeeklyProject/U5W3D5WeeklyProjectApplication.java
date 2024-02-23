0bc639737e23ca4bbd3c74ffe00717d2b77036c8
Daniele Cagnoni <145670818+Sneakydan7@users.noreply.github.com>
2024-02-23 10:39:56 +0100

    Initial commit

---
+++ src/main/java/com/cagnoni/U5W3D5WeeklyProject/U5W3D5WeeklyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cagnoni.U5W3D5WeeklyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5WeeklyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5WeeklyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cagnoni/U5W3D5WeeklyProject/U5W3D5WeeklyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagnoni.U5W3D5WeeklyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5WeeklyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
